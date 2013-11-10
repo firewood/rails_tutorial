5aba46d3e49bcbce3d9fe9b0ece82a2008724526
firewood <karamaki@gmail.com>
2013-11-10 23:48:26 +0900

    dynamic title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,7 +5,13 @@ describe "StaticPages" do
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+          :text => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
