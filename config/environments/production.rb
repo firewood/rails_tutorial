a1df782573f5dd27d7c631ac98be916ce573815b
firewood <karamaki@gmail.com>
2013-11-18 17:35:26 +0900

    heroku fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,5 +68,6 @@ SampleApp::Application.configure do
   # for heroku
   config.logger = Logger.new(STDOUT)
   config.logger.level = Logger.const_get(ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].upcase : 'INFO')
+  config.assets.initialize_on_precompile = false
 
 end
