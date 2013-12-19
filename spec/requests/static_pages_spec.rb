89636d1ab7e2cd1e2845dbb3b20641c5c7d46ea8
firewood <karamaki@gmail.com>
2013-12-20 01:55:11 +0900

    5.6

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,11 +3,18 @@ require 'spec_helper'
 describe "StaticPages" do
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector 'h1',    text: 'Sample App' }
+    it { should have_selector 'title', text: full_title(page_title) }
+  end
+
   describe "Home page" do
     before { visit root_path }
+    let(:heading) { 'Sample App' }
+    let(:page_title) { '' }
 
-    it { should have_selector 'h1',    text: 'Sample App' }
-    it { should have_selector 'title', text: full_title('') }
+    it_should_behave_like "all static pages"
+    it { should_not have_selector 'title', text: '| Home' }
   end
 
   describe "Help page" do
@@ -30,6 +37,15 @@ describe "StaticPages" do
     it { should have_selector('h1',    text: 'Contact') }
     it { should have_selector('title', text: full_title('Contact')) }
   end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+  end
+
+
 
 #  describe "GET /static_pages" do
 #    it "works! (now write some real specs)" do
